b131160e5bb17f186fc616ce4e1567a7c440a9eb
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2020-08-10 19:37:57 -0700

    added folders, changed app to homepage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const Homepage = (props: Props) => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default Homepage
