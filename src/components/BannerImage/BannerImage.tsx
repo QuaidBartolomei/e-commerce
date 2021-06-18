a1764b4182c94d64297dcb93fd1de7819120564e
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-06-17 20:16:38 -0700

    preparing to move to next js

--- src/components/BannerImage/BannerImage.tsx
+++ src/components/BannerImage/BannerImage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import bannerImage from './banner.jpg'
+import bannerImage from './banner.jpg';
+import { Paper } from '@material-ui/core';
+
+const gradientColors: string[] = ['#7cbbe691', '#000000bf'];
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -12,17 +15,17 @@ const useStyles = makeStyles(theme =>
     },
     header: {
       height: 300,
-      width: '100vw',
+      width: '100%',
       overflow: 'hidden',
-      background: ` linear-gradient(to bottom, #bdc3c790, #2c3e5090), url("${bannerImage}")`,
+      background: ` linear-gradient(to top, ${gradientColors.join(
+        ','
+      )}), url("${bannerImage}")`,
+      backgroundPosition: 'center',
     },
   })
 );
 
 export default function BannerImage() {
   const classes = useStyles();
-  return (
-    <div className={classes.header}>
-    </div>
-  );
+  return <Paper elevation={1} square={true} className={classes.header}></Paper>;
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,37 +2,41 @@ import AppBar from '@material-ui/core/AppBar';
 import createStyles from '@material-ui/core/styles/createStyles';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import SignInButton from 'components/SignInButton';
 import React from 'react';
 import NavbarTitle from './NavbarTitle';
 import NavigationDrawerMenu from './NavigationMenu';
 import ShoppingCartIconButton from './ShoppingCartIcon';
 
-const useStyles = makeStyles((theme) =>
+const useStyles = makeStyles(theme =>
   createStyles({
     toolbar: {
+      color: 'white',
     },
   })
 );
- 
-const Navbar = () => {
-  const classes = useStyles();
 
-  const NavbarContent = () => (
-    <Toolbar className={classes.toolbar}>
-      <NavigationDrawerMenu />
-      <NavbarTitle />
-      <SignInButton />
-      <ShoppingCartIconButton />
-    </Toolbar>
-  );
+const Navbar = () => {
+  const scrollTrigger = useScrollTrigger({
+    disableHysteresis: true,
+  });
+  const classes = useStyles({ scrollTrigger });
 
   return (
     <React.Fragment>
-      <AppBar position='fixed' color='primary'>
-        <NavbarContent />
+      <AppBar
+        position='fixed'
+        elevation={scrollTrigger ? 1 : 0}
+        color={scrollTrigger ? 'primary' : 'transparent'}
+      >
+        <Toolbar className={classes.toolbar}>
+          <NavigationDrawerMenu />
+          <NavbarTitle />
+          <SignInButton />
+          <ShoppingCartIconButton />
+        </Toolbar>
       </AppBar>
-      <Toolbar />
     </React.Fragment>
   );
 };
